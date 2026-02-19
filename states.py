ed27f1ca9825318b43451779a2f3ef27162ea43c
PrimeMorph <prime.god.of.transformers@gmail.com>
2026-02-19 21:13:45 +0500

    Update states.py

--- states.py
+++ states.py
@@ -8,3 +8,18 @@ class PhysicsStates(StatesGroup):
     viewing_formula = State()       # просмотр конкретной формулы
     viewing_example = State()       # просмотр примера
     taking_test = State()           # прохождение теста
+
+class AdminStates(StatesGroup):
+    """Состояния для админ-панели"""
+    choosing_action = State()
+    adding_section = State()
+    adding_topic_choose_section = State()
+    adding_topic_name = State()
+    adding_topic_theory = State()
+    adding_formula_choose_topic = State()
+    adding_formula_name = State()
+    adding_formula_text = State()
+    adding_formula_desc = State()
+    adding_example_choose_topic = State()
+    adding_example_question = State()
+    adding_example_answer = State()
